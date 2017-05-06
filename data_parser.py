65d9a38595bf115a2c992c617b5aa1766ad3dff2
Porrith Suong <psuong95@gmail.com>
2017-05-06 16:46:35 -0400

    Adds the parsed data from pystock.

--- data_parser.py
+++ data_parser.py
@@ -8,6 +8,9 @@ RAW_DATA_DIR = "unparsed_data"
 
 
 def parse_data(path: str, company_symbol: [], target_file: str):
+    with open(target_file, "a") as file:
+        writer = csv.writer(file)
+        writer.writerow(["SYMBOL", "CLOSING PRICE", "DAY", "DATE"])
     root = os.path.dirname(os.path.realpath(__file__))
     for path, subdirs, files in os.walk(root):
         for name in files:
@@ -16,26 +19,23 @@ def parse_data(path: str, company_symbol: [], target_file: str):
                 write_to_csv(file_path, company_symbol, target_file)
 
 
-def write_to_csv(read_file: str, company_symbol: [], file_name: str):
+def write_to_csv(read_file: str, company_symbol, file_name: str):
     csv_to_read = csv.reader(open(read_file, newline=""))
     for row in csv_to_read:
         # Skip the first row in the csv file
         if "symbol" in row:
-            with open(file_name, "a") as file:
-                writer = csv.writer(file)
-                writer.writerow(["SYMBOL", "CLOSING PRICE", "DAY", "DATE"])
             continue
 
-        if company_symbol[0] in row[0] or company_symbol[1] in row[0]:
-            current_date = row[1] # Get the current day
-            split_date = current_date.split("-") # Split the date into an array of years, month, days
-            # Create the datetime object
-            current_datetime = datetime(year=int(split_date[0]), month=int(split_date[1]), day=int(split_date[2]))
-            # Determine the type of day
-            weekday = current_datetime.weekday()
-            closing_price = row[5] # Get the closing price
-
-            with open(file_name, "a") as file:
+        with open(file_name, "a") as file:
+            if company_symbol in row:
+                current_date = row[1] # Get the current day
+                print(current_date)
+                split_date = current_date.split("-") # Split the date into an array of years, month, days
+                # Create the datetime object
+                current_datetime = datetime(year=int(split_date[0]), month=int(split_date[1]), day=int(split_date[2]))
+                # Determine the type of day
+                weekday = current_datetime.weekday()
+                closing_price = row[5] # Get the closing price
                 fields = [company_symbol, closing_price, weekday, current_date]
                 writer = csv.writer(file)
                 writer.writerow(fields)
