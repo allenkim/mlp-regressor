8b90501400eaf66d8f0baa8cc538c1c9072724aa
Porrith Suong <psuong95@gmail.com>
2017-05-06 22:06:07 -0400

    Adds a file manager to open and close the file.

--- yielder.py
+++ yielder.py
@@ -2,17 +2,26 @@ from os.path import join
 
 
 class FileManager(object):
-    """
-    Manages the files.
-    """
     def __init__(self):
         self._data_dir = "data"
         self.managed_file = None
 
     def open_file(self, path):
+        """
+        Opens and returns the CSV file.
+        :param path: File to read.
+        :return: The opened file
+        """
         file_path = join(self._data_dir, path)
         file = open(file_path, "r")
+        self.managed_file = file
         return file
+
+    def close_file(self):
+        try:
+            self.managed_file.close()
+        except FileExistsError:
+            print("File not found!")
 
 
 class Yielder(object):
